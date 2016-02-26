c573758b5b8f2763d1918626eadf6ff5a84844c2
lx <494541654@qq.com>
2016-02-26 15:57:32 +0800

    虽然对方
    、了；

---
+++ 11/11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 Lx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 11/11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 Lx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
